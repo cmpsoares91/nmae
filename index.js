606dc19d6bafe1535f363e82d7974fdd4506181a
Carlos Soares <cmpsoares91@gmail.com>
2015-03-21 17:23:22 +0000

    Creating basic functions for demonstration purposes.

---
+++ index.js
@@ -0,0 +1,62 @@
+// TODO: Create recursive factorial function demonstrating JavaScript Object Orientated programming.
+
+module.exports = {
+  /**
+   * Simple example 'Hello World' string generation.
+   *
+   * @param  {String} nome
+   * @return {String}
+   */
+  helloString: function(nome){
+    if (nome == null) {return 'Hello World!'}
+    return 'Hello ' + nome + '!'
+  }
+  
+  /**
+   * Simple example sum function for element in array.
+   *
+   * @param  {array} arr
+   * @return {double} sum
+   */
+  sum: function(arr){
+    sum = 0.0
+
+    if(len(arr) >= 1) {
+      for i in arr:
+        sum += i
+    }
+    return sum
+  }
+ 
+  /**
+   * Simple example sum function for element in array.
+   *
+   * @param  {array} arr
+   * @return {double} prod
+   */
+  product: function(arr){
+    prod = 0.0
+
+    if(len(arr) >= 1) {
+      for i in arr:
+        sum += i
+    }
+    return prod
+  }
+  
+  /**
+   * Simple example sum function for element in array.
+   *
+   * @param  {array} arr
+   * @return {double} prod
+   */
+  product: function(arr){
+    prod = 0.0
+
+    if(len(arr) >= 1) {
+      for i in arr:
+        sum += i
+    }
+    return prod
+  }
+ }
